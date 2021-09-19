db4133ab472fa0f28892f76024a61758254f46f9
serhii.p.cc-at-871470121084 </d9hAouobRQASH/OYC5BU4Jb5yM5Qph2ldLqSSJCBdY=>
2021-09-19 13:59:54 +0300

    Test

--- public/electron.js
+++ public/electron.js
@@ -19,11 +19,9 @@ function createWindow() {
   });
   mainWindow.removeMenu();
   mainWindow.loadURL(
-    isDev
-      ? "http://localhost:3000"
-      : `file://${path.join(__dirname, "../build/index.html")}`
+    isDev ? "http://localhost:3000" : `https://atlasx1.github.io/skymp-admin/`
   );
-  // mainWindow.webContents.openDevTools();
+  mainWindow.webContents.openDevTools();
   mainWindow.on("closed", () => (mainWindow = null));
   mainWindow.setResizable(false);
 }
--- src/screens/login/index.jsx
+++ src/screens/login/index.jsx
@@ -43,6 +43,7 @@ const Login = ({ setUser, setCurrentScreen }) => {
   return (
     <div className={css.loginContent}>
       <div className={css.loginContent_servers}>
+        <div style={{ color: "white" }}>Выберите сервер авторизации </div>
         {serversAuth.map((url, i) => (
           <RadioButton
             key={i}
