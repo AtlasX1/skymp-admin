d8d8d09332fc164cce73fbe6e4c1ddc56db0a8b6
serhii.p.cc-at-871470121084 </d9hAouobRQASH/OYC5BU4Jb5yM5Qph2ldLqSSJCBdY=>
2021-09-19 12:32:22 +0300

    Updates

---
+++ precache-manifest.e9e916f947d5f6ed067cdb0b25da9c00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6d7284fcc567ad3ab9000ae95eaace6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7667c9ed4519328f8697",
+    "url": "./static/css/main.9c646aa6.chunk.css"
+  },
+  {
+    "revision": "f0707888df24f9b951fa",
+    "url": "./static/js/2.aa1ec7be.chunk.js"
+  },
+  {
+    "revision": "7667c9ed4519328f8697",
+    "url": "./static/js/main.08074f20.chunk.js"
+  },
+  {
+    "revision": "163d5d2807617bf8607a",
+    "url": "./static/js/runtime-main.18bf688f.js"
+  },
+  {
+    "revision": "452efa0b6f19894db22b89da83e449e5",
+    "url": "./static/media/close.452efa0b.svg"
+  },
+  {
+    "revision": "a9f0ebd7e3c1e2a41562fed054f3432e",
+    "url": "./static/media/hide.a9f0ebd7.svg"
+  }
+]);
